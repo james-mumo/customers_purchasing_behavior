f152695719c64a212bfcd6c1258e3f1bcdea0794
wildcard98 <98mumo@gmail.com>
2024-02-06 03:31:43 +0300

    new

--- eda.R
+++ eda.R
@@ -8,6 +8,8 @@
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(psych)
+library(skimr)
 
 
 # Read CSV file into a data frame
@@ -184,7 +186,144 @@ str(customer_df)
 
 ## DATA Preparation
 
+## AGE Group Preparations
+
 # Creating Age Groups
 customer_df$Age_Group <- cut(customer_df$Customer.Age, breaks = c(17, 25, 35, 45, 60, Inf), labels = c("18-25", "26-35", "36-45", "46-60", "60+"))
 str(customer_df)
+
+
+# Group by Age_Group Count The Number of People in Each Group
+
+unique_people_count_by_age <- customer_df %>%
+  group_by(Age_Group) %>%
+  summarise(Number_of_Unique_People = n_distinct(Customer.ID))
+
+# Print the result
+print(unique_people_count_by_age)
+
+
+
+# Summing up the total number of people across all age groups
+total_people_count <- sum(unique_people_count_by_age$Number_of_Unique_People)
+
+# Print the total result
+print(total_people_count)
+
+
+# Group by Age_Group and calculate the total purchase amount for each group
+total_purchase_by_age <- customer_df %>%
+  group_by(Age_Group) %>%
+  summarise(Total.Purchase.Amount = sum(Total.Purchase.Amount))
+
+# Print the result
+print(total_purchase_by_age)
+
+## Get the Products Categories
+unique_product_categories <- unique(customer_df$Product.Category)
+# Print the unique values
+print(unique_product_categories)
+
+
+# Filter the data for the first category ("Home") and then group by Age_Group
+sales_first_category_by_Home <- customer_df %>%
+  filter(Product.Category == "Home") %>%
+  group_by(Age_Group) %>%
+  summarise(Total.Purchase.Amount_Home = sum(Total.Purchase.Amount))
+
+# Print the result
+print(sales_first_category_by_Home)
+
+
+
+
+# Filter the data for the first category ("Electronics") and then group by Age_Group
+sales_first_category_by_Electronics <- customer_df %>%
+  filter(Product.Category == "Electronics") %>%
+  group_by(Age_Group) %>%
+  summarise(Total.Purchase.Amount_Electronics = sum(Total.Purchase.Amount))
+
+# Print the result
+print(sales_first_category_by_Electronics)
+
+
+
+
+
+
+# Filter the data for the first category ("Books") and then group by Age_Group
+sales_first_category_by_Books <- customer_df %>%
+  filter(Product.Category == "Books") %>%
+  group_by(Age_Group) %>%
+  summarise(Total.Purchase.Amount_Books = sum(Total.Purchase.Amount))
+
+# Print the result
+print(sales_first_category_by_Books)
+
+
+# Filter the data for the first category ("Clothing") and then group by Age_Group
+sales_first_category_by_Clothing <- customer_df %>%
+  filter(Product.Category == "Clothing") %>%
+  group_by(Age_Group) %>%
+  summarise(Total.Purchase.Amount_Clothing = sum(Total.Purchase.Amount))
+
+# Print the result
+print(sales_first_category_by_Clothing)
+
+
+
+## GENDER PREPARATIONS
+
+# Create Total Purchase Amount based on gender groups
+total_purchase_gender <- aggregate(Total.Purchase.Amount ~ Gender, data = customer_df, sum)
+
+# Display the result
+total_purchase_gender
+
+# Calculate the total sum of the "Total_Purchase" column
+total_purchase_sum <- sum(customer_df$Total.Purchase.Amount)
+
+# Display the result
+total_purchase_sum
+
+
+# Filter data for the "Home" category
+home_data <- subset(customer_df, Product.Category == "Home")
+
+# Create Total Purchase Amount based on gender groups in the "Home" category
+total_purchase_home_based_on_gender <- aggregate(Total.Purchase.Amount ~ Gender, data = home_data, sum)
+
+# Display the result
+total_purchase_home_based_on_gender
+
+
+# Filter data for the "Electronics" category
+electronics_data <- subset(customer_df, Product.Category == "Electronics")
+
+# Create Total Purchase Amount based on gender groups in the "Electronics" category
+total_purchase_electronics_gender <- aggregate(Total.Purchase.Amount ~ Gender, data = electronics_data, sum)
+
+# Display the result for Electronics
+total_purchase_electronics_gender
+
+
+# Filter data for the "Clothing" category
+clothing_data <- subset(customer_df, Product.Category == "Clothing")
+
+# Create Total Purchase Amount based on gender groups in the "Clothing" category
+total_purchase_clothing_gender <- aggregate(Total.Purchase.Amount ~ Gender, data = clothing_data, sum)
+
+# Display the result for Clothing
+total_purchase_clothing_gender
+
+
+# Filter data for the "Books" category
+books_data <- subset(customer_df, Product.Category == "Books")
+
+# Create Total Purchase Amount based on gender groups in the "Books" category
+total_purchase_books_gender <- aggregate(Total.Purchase.Amount ~ Gender, data = books_data, sum)
+
+# Display the result for Books
+total_purchase_books_gender
+
 
